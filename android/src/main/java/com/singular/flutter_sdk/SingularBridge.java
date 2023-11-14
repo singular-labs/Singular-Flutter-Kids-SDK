6803f83fa7525f704cd4c58fc04a1e91f02359ff
nur nachman <nur@singular.net>
2023-11-14 16:29:36 +0200

    rename folder

--- android/src/main/java/com/singular/flutter_sdk/SingularBridge.java
+++
@@ -1,9 +0,0 @@
-package com.singular.flutter_kids_sdk;
-
-import android.content.Intent;
-
-public class SingularBridge {
-    public static void onNewIntent(Intent intent) {
-        SingularSDK.onNewIntent(intent);
-    }
-}
